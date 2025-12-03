5585947674156d3271bae3c1f9d32d14b8cf00e1
liel levi <liel@levi.com>
2025-12-03 23:11:12 +0200

    cicd

--- read_pdf.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader
-
-reader = PdfReader("DevOps_Final_Project_Requirements (2).pdf")
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-
-with open("requirements_text.txt", "w", encoding="utf-8") as f:
-    f.write(text)
